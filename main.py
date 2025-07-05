c48338d5e197e2fb179ffb6f903bfe18e81ab38d
d1233d <d1233d@qq.com>
2025-07-05 19:38:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def proxy_tts():
             Bucket=AWS_BUCKET,
             Key=s3_key,
             Body=audio_bytes,
-            ContentType="audio/{data.get('format', 'mp3')}"
+            ContentType="audio/mpeg"
         )
 
         audio_url = f"{AWS_S3_DOMAIN}/{s3_key}"
